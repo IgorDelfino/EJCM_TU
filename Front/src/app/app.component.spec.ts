5df9e968530cfec681d8b366d5136f2cacda5a33
Tef Xeb <ateka.zach.09@gmail.com>
2020-05-03 19:00:32 -0300

    +teste funcionando

--- Front/src/app/app.component.spec.ts
+++ Front/src/app/app.component.spec.ts
@@ -1,8 +1,46 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { fakeAsync, tick, ComponentFixture } from '@angular/core/testing';
+import { NO_ERRORS_SCHEMA, ɵivyEnabled as ivyEnabled } from '@angular/core';
 
-describe('AppComponent', () => {
+
+
+// describe('AppComponent', () => {
+//   beforeEach(async(() => {
+//     TestBed.configureTestingModule({
+//       imports: [
+//         RouterTestingModule
+//       ],
+//       declarations: [
+//         AppComponent
+//       ],
+//     }).compileComponents();
+//   }));
+
+//   it('should create the app', () => {
+//     const fixture = TestBed.createComponent(AppComponent);
+//     const app = fixture.componentInstance;
+//     expect(app).toBeTruthy();
+//   });
+
+//   it(`should have as title 'TUPaper'`, () => {
+//     const fixture = TestBed.createComponent(AppComponent);
+//     const app = fixture.componentInstance;
+//     expect(app.title).toEqual('TUPaper');
+//   });
+
+//   it('should render title', () => {
+//     const fixture = TestBed.createComponent(AppComponent);
+//     fixture.detectChanges();
+//     const compiled = fixture.nativeElement;
+//     expect(compiled.querySelector('.content span').textContent).toContain('TUPaper app is running!');
+//   });
+// });
+
+
+describe('Teste para UI', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
@@ -11,25 +49,12 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
-    }).compileComponents();
+  }).compileComponents();
   }));
+  // let component: AppComponent;
+    it('Deve renderizar mensagem no h1', async(() => {
+      const fixture = TestBed.createComponent(AppComponent);
+      const compiled = fixture.debugElement.nativeElement;
+      expect(compiled.querySelector('h1').textContent).toContain('helloworld');
+  }))})
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'TUPaper'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('TUPaper');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('TUPaper app is running!');
-  });
-});
--- Front/src/setupJest.ts
+++ Front/src/setupJest.ts
@@ -1,2 +1,5 @@
 import 'jest-preset-angular';
 import './jestGlobalMocks'; // browser mocks globally available for every test
+//import {describe, expect, it} from '@jest/globals'
+
+
