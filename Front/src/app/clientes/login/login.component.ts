7787a8917698961ad2538990fcf13172bc8206f2
Tef Xeb <ateka.zach.09@gmail.com>
2020-05-27 18:17:55 -0300

    form login inicializado

---
+++ Front/src/app/clientes/login/login.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  loginForm: FormGroup;
+
+  constructor(public formbuilder: FormBuilder) {
+
+    this.loginForm = this.formbuilder.group({
+      email: [null, [Validators.required, Validators.email]],
+      password: [null, [Validators.required, Validators.minLength(6)]]
+    })
+   }
+
+  ngOnInit(): void {
+  }
+
+}
