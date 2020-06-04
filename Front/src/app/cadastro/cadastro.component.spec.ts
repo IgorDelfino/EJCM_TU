67d736ac6e41d3bb39037426954d610699b266d2
Tef Xeb <ateka.zach.09@gmail.com>
2020-06-03 22:54:48 -0300

    + adição do validator no form e e implementação do teste cadastro

--- Front/src/app/cadastro/cadastro.component.spec.ts
+++ Front/src/app/cadastro/cadastro.component.spec.ts
@@ -1,25 +1,54 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormBuilder } from "@angular/forms";
 
 import { CadastroComponent } from './cadastro.component';
 
-describe('CadastroComponent', () => {
-  let component: CadastroComponent;
-  let fixture: ComponentFixture<CadastroComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ CadastroComponent ]
-    })
-    .compileComponents();
-  }));
-
+describe('CadastroComponent', () => { 
+ 
+  let cadastroComponent: CadastroComponent;
+ 
   beforeEach(() => {
-    fixture = TestBed.createComponent(CadastroComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+ 
+    cadastroComponent = new CadastroComponent(new FormBuilder);
+ 
+  })
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+  it('Deve verificar se contém os itens no form', () => {
+
+    expect(cadastroComponent.registerForm.contains('name')).toBe(true);
+    expect(cadastroComponent.registerForm.contains('password')).toBeTruthy();
+    expect(cadastroComponent.registerForm.contains('email')).toBeTruthy();
+   
+    })
+
+  it('should make the name control required', () => {
+
+    let nameControl = cadastroComponent.registerForm.get('name');
+   
+    nameControl.setValue('');
+     
+    expect(nameControl.valid).toBeFalsy();
+     
+    })
+
+  it('should use password with minimum 8 characters', () => {
+
+    let passwordControl = cadastroComponent.registerForm.get('password');
+       
+    passwordControl.setValue('12345678')
+       
+    expect(passwordControl.valid).toBeTruthy();
+       
+    })
+       
+  it('should validate the email input type', () => {
+       
+    let emailControl = cadastroComponent.registerForm.get('email');
+       
+    emailControl.setValue('dinanathj@gmail.com')
+       
+    expect(emailControl.valid).toBeTruthy();
+       
+})});
+       
+   
