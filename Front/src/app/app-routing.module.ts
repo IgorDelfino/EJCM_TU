196edb0c4ed266b8dff478cd0c18978a8c24ab0f
bellav0 <isabellaraymundo38@gmail.com>
2020-06-04 14:22:04 -0300

    + header na pagina home/produto

--- Front/src/app/app-routing.module.ts
+++ Front/src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ProdutoComponent } from './produto/produto.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
 
 
-const routes: Routes = [{ path: 'cadastro', component: ProdutoComponent }];
+const routes: Routes = [{ path: 'cadastro', component: CadastroComponent },{ path:'produto', component:ProdutoComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Front/src/app/cadastro/cadastro.component.spec.ts
+++ Front/src/app/cadastro/cadastro.component.spec.ts
@@ -1,25 +1,68 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { FormBuilder } from '@angular/forms';
 import { CadastroComponent } from './cadastro.component';
 
-describe('CadastroComponent', () => {
-  let component: CadastroComponent;
-  let fixture: ComponentFixture<CadastroComponent>;
+describe('LoginFormComponent', () => { 
 
+  let component: CadastroComponent;
+ 
+  beforeEach(() => {
+ 
+  component = new CadastroComponent(new FormBuilder);
+ 
+  })
+ 
+  it('should create a form with 3 controls', () => {
+ 
+  expect(component.registerForm.contains('name')).toBe(true);
+  expect(component.registerForm.contains('password')).toBeTruthy();
+  expect(component.registerForm.contains('email')).toBeTruthy();
+ 
+  })
+
+  it('should make the name control required', () => {
+
+    let nameControl = component.registerForm.get('name');
+   
+    nameControl.setValue('');
+   
+    expect(nameControl.valid).toBeFalsy();
+   
+    })
+   
+    it('should use password with minimum 8 characters', () => {
+   
+    let passwordControl = component.registerForm.get('password');
+   
+    passwordControl.setValue('12345678')
+   
+    expect(passwordControl.valid).toBeTruthy();
+   
+    })
+   
+    it('should validate the email input type', () => {
+   
+    let emailControl = component.registerForm.get('email');
+   
+    emailControl.setValue('dinanathj@gmail.com')
+   
+    expect(emailControl.valid).toBeTruthy();
+   
+    })
+})
+
+describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CadastroComponent ]
-    })
-    .compileComponents();
+      declarations: [
+        CadastroComponent
+      ],
+    }).compileComponents();
   }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(CadastroComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  it('should create the app', async(() => {
+    const fixture = TestBed.createComponent(CadastroComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
+  }));
 });
