2925040c2e242d0f2fc92c301bb36ca2c8093968
Julia Lopes <jujulrocha@gmail.com>
2020-06-17 02:44:39 -0300

    APP para teste

--- Front/src/app/app.component.spec.ts
+++ Front/src/app/app.component.spec.ts
@@ -1,15 +1,9 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
-import { ComponentFixture } from '@angular/core/testing';
-import { NO_ERRORS_SCHEMA, ɵivyEnabled as ivyEnabled } from '@angular/core';
 
-
-describe('Teste para UI', () => {
-  let component: AppComponent;
-  let equipamento: ComponentFixture<AppComponent>;
-
-beforeEach(async(() => {
+describe('AppComponent', () => {
+  beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
         RouterTestingModule
@@ -17,86 +11,26 @@ beforeEach(async(() => {
       declarations: [
         AppComponent
       ],
-      schemas: [ NO_ERRORS_SCHEMA ],
-  }).compileComponents();
+    }).compileComponents();
   }));
-  
-  beforeEach(() => {
-        equipamento = TestBed.createComponent(AppComponent);
-        component = equipamento.componentInstance;
-        equipamento.detectChanges();
-      });
-    
-     it('Deve renderizar mensagem no h1', async(() => {
-      const equipamento = TestBed.createComponent(AppComponent);
-      const compiled = equipamento.debugElement.nativeElement;
-      expect(compiled.querySelector('h1').textContent).toEqual('TU Paper');
-  }));
-  
-  it('Deve tirar o snapshot', () => {
-    expect(equipamento).toMatchSnapshot();
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have as title 'TUPaper'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('TUPaper');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('.content span').textContent).toContain('TUPaper app is running!');
+  });
 });
-})
-  
-  
 
-// describe('AppComponent', () => {
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       imports: [
-//         RouterTestingModule
-//       ],
-//       declarations: [
-//         AppComponent
-//       ],
-//     }).compileComponents();
-//   }));
-
-//   it('should create the app', () => {
-//     const equipamento = TestBed.createComponent(AppComponent);
-//     const app = equipamento.componentInstance;
-//     expect(app).toBeTruthy();
-//   });
-
-//   it(`should have as title 'TUPaper'`, () => {
-//     const equipamento = TestBed.createComponent(AppComponent);
-//     const app = equipamento.componentInstance;
-//     expect(app.title).toEqual('TUPaper');
-//   });
-
-//   it('should render title', () => {
-//     const equipamento = TestBed.createComponent(AppComponent);
-//     equipamento.detectChanges();
-//     const compiled = equipamento.nativeElement;
-//     expect(compiled.querySelector('.content span').textContent).toContain('TUPaper app is running!');
-//   });
-// });
-
-//--------------------------------------------------------------------------
-
-// import { async, Componentequipamento, TestBed } from '@angular/core/testing';
-
-// import { SimpleWithStylesComponent } from './simple-with-styles.component';
-// import { ChildComponent } from '../medium/child.component';
-
-// describe('SimpleWithStylesComponent', () => {
-//   let component: SimpleWithStylesComponent;
-//   let equipamento: Componentequipamento<SimpleWithStylesComponent>;
-
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [ SimpleWithStylesComponent, ChildComponent ]
-//     })
-//     .compileComponents();
-//   }));
-
-//   beforeEach(() => {
-//     equipamento = TestBed.createComponent(SimpleWithStylesComponent);
-//     component = equipamento.componentInstance;
-//     equipamento.detectChanges();
-//   });
-
-//   it('should generate snapshot without nghost/ngcontent', () => {
-//     expect(equipamento).toMatchSnapshot();
-//   });
-// });
--- Front/src/app/produto/produto.component.ts
+++ Front/src/app/produto/produto.component.ts
@@ -30,11 +30,7 @@ export class ProdutoComponent implements OnInit {
         estoque: 20
       },
       {
-<<<<<<< HEAD
         descricao:" Estojo de Canetinha Hidrográfica 24 Cores Faber Castell 1UN ",
-=======
-        descricao:"Canetinhas Hidrográficas 24 Cores Faber Castell 1UN ",
->>>>>>> 2e49dd529c727ad7b440e7fb212d2b82ce386703
         preco:13,
         img:"../../assets/produtos/canetinha.jpg",
         estoque: 30
@@ -61,27 +57,6 @@ export class ProdutoComponent implements OnInit {
     
   }
    
-   alteraEstoque(produto){
-       produto.estoque --
-    }
-
-    casaFunction (items, callback) {
-      for (let index = 0; index < items.length; index++) {
-        callback(items[index]);
-      }
-    }
-    
-    Sum (a, b){
-      return a + b;
-    }
-
-    toggle () {
-      this.toggleCompra = !this.toggleCompra;
-    }
-   
-  vaiParaLogin(){
-    this.router.navigate(['/login'])
-  }
 }
 
 
